5a9244319d8e149fd6c24b5f4af0ce3153c43052
edgar.carrillo <issedgar@hotmail.com>
2021-04-16 11:31:38 -0500

    version 0.0.1

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+    /**
+     * Suma de dos numeros
+     * @example
+     * n1 = 1, n2 = 3 => resultado 4
+     * @param {*} n1 Numero uno de la suma
+     * @param {*} n2 Numero dos de la suma
+     * @returns 
+     * number
+     */
+    suma: function(n1, n2) {
+        return n1 + n2;
+    },
+    resta: function(n1, n2) {
+        return n1 - n2;
+    },
+    multiplicacion: function(n1, n2) {
+        return n1 * n2;
+    },
+    division: function(n1, n2) {
+        return n1 / n2;
+    }
+}
---
+++ pruebas.js
@@ -0,0 +1,6 @@
+const m = require('.');
+
+console.log(m.suma(3, 8));
+
+
+console.log(m.multiplicacion(3, 8));
